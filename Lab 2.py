e9e9fe884f2d8ed2942fa51c680775c7bfd2d7e6
gantip0907 <gantip0907@students.bowiestate.edu>
2023-09-21 16:13:20 -0400

    Lab 1 + 2

---
+++ Lab 2.py
@@ -0,0 +1,5 @@
+print("Hello my name is Preetam")
+
+print("I am a student in the MIS graduate program at Bowie State University")
+
+print("I am currently enrolled in four classes: INSS 505, INSS 538, INSS 540, and INSS 675")
