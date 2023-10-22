c8ae7d797dfc00df72afddb6ee053995d822ed92
gantip0907 <GANTIP0907@students.bowiestate.edu>
2023-10-22 14:59:39 -0400

    Lab 3

---
+++ Lab3.py
@@ -0,0 +1,54 @@
+import math
+
+number1 = int(input ('Enter number 1 '))
+number2 = int(input ('Enter number 2 '))
+number3 = int(input ('Enter number 3 '))
+
+sum=number1+number2+number3
+
+difference= number1-number2-number3
+
+multiplication= number1*number2*number3
+
+division=(number1/number2)/number3
+
+print (sum, difference, multiplication, division)
+
+print (math.pow(9,3))
+
+
+import math
+
+
+import math
+
+# Square root of 25
+print("Square root:", math.sqrt(25))
+
+# Sine of 45 degrees
+print("Sine:", math.sin(math.radians(45)))
+
+# Cosine of 60 degrees
+print("Cosine:", math.cos(math.radians(60)))
+
+# Tangent of 30 degrees
+print("Tangent:", math.tan(math.radians(30)))
+
+# Logarithm base 10 of 1000
+print("Log base 10:", math.log10(1000))
+
+# Exponential of 2
+print("Exponential:", math.exp(2))
+
+# Floor division of 15 by 4
+print("Floor division:", 15 // 4)
+
+# Ceiling of 3.14
+print("Ceiling:", math.ceil(3.14))
+
+# Factorial of 4
+print("Factorial:", math.factorial(4))
+
+# Absolute value of -7
+print("Absolute value:", abs(-7))
+
