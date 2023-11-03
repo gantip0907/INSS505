122c2bacae84b9aa81a8a64c8afb67503643d77b
gantip0907 <GANTIP0907@students.bowiestate.edu>
2023-11-03 15:55:28 -0400

    Lab 3

---
+++ Problem 3.py
@@ -0,0 +1,15 @@
+number1 = int(input("Enter your first number: "))
+
+number2 = int(input("Enter your second number: "))
+
+number3 = int(input("Enter your third number: "))
+
+number4 = int(input("Enter your fourth number: "))
+
+number5 = int(input("Enter your fifth number: "))
+
+sum = number1+number2+number3+number4+number5
+print ("The sum of all your numbers is: ", sum)
+
+average = sum / 5
+print ("The average of all your numbers is: ", average)
---
+++ Problem 4.py
@@ -0,0 +1,4 @@
+# I created a for loop to print the squares of numbers from 1 to 10
+for num in range(1, 11):
+    square = num**2
+    print(f"The square of {num} is: {square}")
